eb6f17a254b4707c0dc99cda7e0ac02a693a255d
mgainza <gainza2014@gmail.com>
2026-04-26 14:03:44 +0000

    Add global exception handler

---
+++ src/main/java/com/challenge/transfers/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.challenge.transfers.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> handleUnexpected(Exception ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(new ErrorResponse("INTERNAL_ERROR", "An unexpected error occurred"));
+  }
+}
